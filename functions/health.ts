2c6ff6e306e1d85581338a9b2fd9caf2ef3bc498
Prawira Genestonlia <prawira.96@gmail.com>
2023-12-06 18:39:04 +0800

    feat: function starter (#3)

--- functions/health.ts
+++ functions/health.ts
@@ -1,9 +1,18 @@
 import { EventContext } from '@cloudflare/workers-types'
 import { Env } from '../types'
+import { JsonResponse } from '../utils/json-response'
 
 interface Data {}
 
 export const onRequest = async (context: EventContext<Env, string, Data>) => {
   console.log(context)
-  return new Response(JSON.stringify({ status: 'OK!' }))
+  return new JsonResponse(
+    {
+      status: 'OK!',
+      timestamp: new Date(),
+    },
+    {
+      status: 200,
+    }
+  )
 }
